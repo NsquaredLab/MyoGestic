160072d28154ccc700707d8b32df574c8559f25f
Raul C. Sîmpetru <raul.simpetru@fau.de>
2025-02-25 09:00:27 +0100

    Add myogestic to sys.path in main.py for module accessibility

--- myogestic/main.py
+++ myogestic/main.py
@@ -3,6 +3,11 @@ import sys
 import qdarkstyle
 from PySide6.QtWidgets import QApplication
 
+import os
+
+# add myogestic to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from myogestic.gui.myogestic import MyoGestic
 from utils.config import CONFIG_REGISTRY  # noqa
 
