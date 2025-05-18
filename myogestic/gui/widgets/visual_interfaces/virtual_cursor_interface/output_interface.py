cc837e18f0b9b3aecae0107b45ef9920c3e71642
Vlad Cnejevici <vlad.cnejevici@fau.de>
2025-05-19 00:06:49 +0200

    Modify cursor values from +-100 to +-1

--- myogestic/gui/widgets/visual_interfaces/virtual_cursor_interface/output_interface.py
+++ myogestic/gui/widgets/visual_interfaces/virtual_cursor_interface/output_interface.py
@@ -8,10 +8,10 @@ from myogestic.gui.widgets.visual_interfaces.virtual_cursor_interface.setup_inte
 
 CURSOR_PREDICTION2INTERFACE_MAP = {
     0: (0, 0),
-    1: (0, 100),
-    2: (0, -100),
-    3: (100, 0),
-    4: (-100, 0),
+    1: (0, 1),
+    2: (0, -1),
+    3: (1, 0),
+    4: (-1, 0),
 }
 
 
@@ -30,9 +30,7 @@ class VirtualCursorInterface_OutputSystem(OutputSystemTemplate):
         super().__init__(main_window, prediction_is_classification)
 
         if self._main_window.selected_visual_interface is None:
-            self._main_window.logger.print(
-                "No visual interface selected.", level=LoggerLevel.ERROR
-            )
+            self._main_window.logger.print("No visual interface selected.", level=LoggerLevel.ERROR)
             raise ValueError("No visual interface selected.")
 
         if not isinstance(
@@ -44,9 +42,7 @@ class VirtualCursorInterface_OutputSystem(OutputSystemTemplate):
                 f"Got {type(self._main_window.selected_visual_interface)}."
             )
 
-        self._outgoing_message_signal = (
-            self._main_window.selected_visual_interface.outgoing_message_signal
-        )
+        self._outgoing_message_signal = self._main_window.selected_visual_interface.outgoing_message_signal
 
     def _process_prediction__classification(self, prediction: Any) -> bytes:
         """Process the prediction for classification."""
