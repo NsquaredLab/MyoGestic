e367b7df40c83f1cadab1d86c3e4426d0bfcdfed
Vlad Cnejevici <vlad.cnejevici@fau.de>
2025-05-19 00:03:15 +0200

    Add cursor CatBoost and remove VHI while VCI active

--- myogestic/default_config.py
+++ myogestic/default_config.py
@@ -116,6 +116,52 @@ CONFIG_REGISTRY.register_model(
 )
 
 CONFIG_REGISTRY.register_model(
+    "Cursor CatBoost Classifier",
+    CatBoostClassifier,
+    True,
+    catboost_models.save,
+    catboost_models.load,
+    catboost_models.train,
+    catboost_models.predict,
+    {
+        "iterations": {
+            "start_value": 10,
+            "end_value": 10000,
+            "step": 10,
+            "default_value": 300,
+        },
+        "depth": {
+            "start_value": 1,
+            "end_value": 100,
+            "step": 1,
+            "default_value": 10,
+        },
+        "l2_leaf_reg": {
+            "start_value": 0.001,
+            "end_value": 10,
+            "step": 0.001,
+            "default_value": 0.014,
+        },
+        "border_count": {
+            "start_value": 1,
+            "end_value": 255,
+            "step": 1,
+            "default_value": 254,
+        },
+        "learning_rate": {
+            "start_value": 0.01,
+            "end_value": 10,
+            "step": 0.01,
+            "default_value": 0.04,
+        }
+    },
+    {
+        "task_type": "GPU" if get_gpu_device_count() > 0 else "CPU",
+        "train_dir": None,
+    },
+)
+
+CONFIG_REGISTRY.register_model(
     "AdaBoost Classifier",
     AdaBoostClassifier,
     True,
@@ -240,5 +286,6 @@ CONFIG_REGISTRY.register_visual_interface(
     setup_interface_ui=VirtualCursorInterface_SetupInterface,
     recording_interface_ui=VirtualCursorInterface_RecordingInterface,
 )
-CONFIG_REGISTRY.register_output_system("VHI", VirtualHandInterface_OutputSystem)
+# TODO: uncomment VHI when done
+# CONFIG_REGISTRY.register_output_system("VHI", VirtualHandInterface_OutputSystem)
 CONFIG_REGISTRY.register_output_system("VCI", VirtualCursorInterface_OutputSystem)
